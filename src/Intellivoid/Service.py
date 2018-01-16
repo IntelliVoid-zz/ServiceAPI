1bf0a7a782a6c3cb5f7ed717351c6039b4d850bf
Netkas <30381982+netkas@users.noreply.github.com>
2018-01-16 20:59:28 -0300

    Update Service.py
    Replaced "addQuota" with "increaseQuota"

--- src/Intellivoid/Service.py
+++ src/Intellivoid/Service.py
@@ -83,15 +83,15 @@ class Service:
 		ResponseData = json.loads(Response.text)
 		return(ResponseData['response'])
 
-	def addQuota(self, APIKey, Quota = 500):
-		"""Adds quota to a API Key
+	def increaseQuota(self, APIKey, Quota = 500):
+		"""Increases the quota of a API Key
 		
 		Arguments:
 			APIKey {[type]} -- The public API Key from the consumer
 		
 		Keyword Arguments:
-			Quota {[type]} -- The amount of quota to add to the key (default: {500})
+			Quota {[type]} -- The amount of quota to increase to the key (default: {500})
 		"""
 
-		self.RequestHandler.sendRequest({'action': 'addQuota', 'service_authkey': self.ServiceAuthKey, 'apiKey': APIKey, 'quota': Quota}, True)
+		self.RequestHandler.sendRequest({'action': 'increaseQuota', 'service_authkey': self.ServiceAuthKey, 'apiKey': APIKey, 'quota': Quota}, True)
 		return(True)
